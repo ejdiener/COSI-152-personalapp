f0d7528fc8ce30aceb7e377715ed6c44f73e80ed
ejdiener <64671864+ejdiener@users.noreply.github.com>
2021-06-18 14:27:49 -0400

    initial repo commit

---
+++ models/WISCV.js
@@ -0,0 +1,40 @@
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+var WISCVSchema = Schema( {
+  wisc_vDate: Date,
+  wisc_vSI: Number,
+  wisc_vVC: Number,
+  wisc_vIN: Number,
+  wisc_vCO: Number,
+  wisc_vBD: Number,
+  wisc_vVP: Number,
+  wisc_vMR: Number,
+  wisc_vFW: Number,
+  wisc_vPC: Number,
+  wisc_vAR: Number,
+  wisc_vDS: Number,
+  wisc_vPS: Number,
+  wisc_vLN: Number,
+  wisc_vCD: Number,
+  wisc_vSS: Number,
+  wisc_vCA: Number,
+  wisc_vVCI: Number,
+  wisc_vVSI: Number,
+  wisc_vFRI: Number,
+  wisc_vWMI: Number,
+  wisc_vPSI: Number,
+  wisc_vQRI: Number,
+  wisc_vAWMI: Number,
+  wisc_vNVI: Number,
+  wisc_vGAI: Number,
+  wisc_vCPI: Number,
+  wisc_vNSI: Number,
+  wisc_vSTI: Number,
+  wisc_vSRI: Number,
+  wisc_vFSIQ: Number
+} );
+
+module.exports = mongoose.model( 'WISCV', WISCVSchema );
---
+++ models/wj_iv_achEval.js
@@ -0,0 +1,50 @@
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+var wj_iv_achEvalSchema = Schema( {
+  WJ_IV_ACHdate: Date,
+  WJ_IV_ACH_1: Number,
+  WJ_IV_ACH_2: Number,
+  WJ_IV_ACH_3: Number,
+  WJ_IV_ACH_4: Number,
+  WJ_IV_ACH_5: Number,
+  WJ_IV_ACH_6: Number,
+  WJ_IV_ACH_7: Number,
+  WJ_IV_ACH_8: Number,
+  WJ_IV_ACH_9: Number,
+  WJ_IV_ACH_10: Number,
+  WJ_IV_ACH_11: Number,
+  WJ_IV_ACH_12: Number,
+  WJ_IV_ACH_13: Number,
+  WJ_IV_ACH_14: Number,
+  WJ_IV_ACH_15: Number,
+  WJ_IV_ACH_16: Number,
+  WJ_IV_ACH_17: Number,
+  WJ_IV_ACH_18: Number,
+  WJ_IV_ACH_19: Number,
+  WJ_IV_ACH_20: Number,
+  WJ_IV_ACH_r: Number,
+  WJ_IV_ACH_br: Number,
+  WJ_IV_ACH_brs: Number,
+  WJ_IV_ACH_rc: Number,
+  WJ_IV_ACH_rf: Number,
+  WJ_IV_ACH_rr: Number,
+  WJ_IV_ACH_m: Number,
+  WJ_IV_ACH_bm: Number,
+  WJ_IV_ACH_mcs: Number,
+  WJ_IV_ACH_mps: Number,
+  WJ_IV_ACH_wl: Number,
+  WJ_IV_ACH_bwl: Number,
+  WJ_IV_ACH_bws: Number,
+  WJ_IV_ACH_we: Number,
+  WJ_IV_ACH_as: Number,
+  WJ_IV_ACH_af: Number,
+  WJ_IV_ACH_aa: Number,
+  WJ_IV_ACH_ak: Number,
+  WJ_IV_ACH_pgk: Number,
+  WJ_IV_ACH_ba: Number
+} );
+
+module.exports = mongoose.model( 'wj_iv_achEval', wj_iv_achEvalSchema );
