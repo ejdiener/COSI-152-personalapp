f24448732cc15fee9812cdb53f5f4b266cb2ade2
ejdiener <64671864+ejdiener@users.noreply.github.com>
2021-06-18 14:59:23 -0400

    Update WISC_V.js

--- routes/WISC_V.js
+++ routes/WISC_V.js
@@ -1,6 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const WISCV = require('../models/WISCV')
 
+isLoggedIn = (req,res,next) => {
+  if (res.locals.loggedIn) {
+    next()
+  } else {
+    res.redirect('/login')
+  }
+}
+
+router.get('/',
+  isLoggedIn,
+  async (req, res, next) => {
+      res.locals.items = await WISCV.find({userId:req.user._id})
+      res.render('wiscvHome');
+});
 
 
 module.exports = router;
