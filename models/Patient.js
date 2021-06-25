d9630bcdf664266ac42d3831b87c697a5f3b9f04
ejdiener <64671864+ejdiener@users.noreply.github.com>
2021-06-25 13:26:08 -0400

    Update Patient.js

--- models/Patient.js
+++ models/Patient.js
@@ -11,8 +11,7 @@ var PatientSchema = Schema( {
   patientAddress: String,
   patientPhone: String,
   patientBio: String,
-  patientGuardian: ObjectId,
-  CurrentProvidersIds: [Schema.Types.ObjectId],                 // SCHEMA?? of current providers
+  CurrentProvidersIds: [{type:ObjectId, ref:"Provider"}],                 // SCHEMA?? of current providers
 
   PregBirthHistory: ObjectId,                 // SCHEMA: Stores results of PregBirthHistory form
 //  DevelopmentHistory: ObjectId,               // SCHEMA: Stores results of DevelopmentHistory form
@@ -31,9 +30,3 @@ module.exports = mongoose.model( 'Patient', PatientSchema );
 // Patient stores history (which includes a lot of forms, but only one copy of each one)
   // The whole history can be passed into a history page, where it can be displayed as a whole
     // Both the whole history or the individual parts of the history can be exported to a .doc file
-
-// Patient stores multiple evaluations, which in turn stores a bundle of tests
-  // The whole evaluation can be displayed as a whole, including all test Schemas stored within
-    // Each individual test can also be viewed, but more likely the test will be viewed as part of the whole evaluation
-      // It would almost be best to have the WISC-V and WJ-IV-ACH stored as 'templates' that could be embedded into the display for the evaluation.
-      // Evaluation results as a whole can be exported to a .doc file
