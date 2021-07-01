a7f8fc77e76d7403fb2bb0c53c8f7ddb56a40dc9
ejdiener <64671864+ejdiener@users.noreply.github.com>
2021-07-01 09:47:25 -0400

    Update profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Provider = require('../models/Provider')
+const Patient = require('../models/Patient')
 
 isLoggedIn = (req,res,next) => {
   if (res.locals.loggedIn) {
@@ -14,11 +15,9 @@ router.get('/:providerId',
     isLoggedIn,
     async (req,res,next) => {
       const providerId = req.params.providerId
-      console.log(providerId)
       const provider = await Provider.findOne({userId:providerId})
       res.locals.currUserId = req.user._id
       res.locals.provider = provider
-      console.log(provider)
       res.render('profileProvider')
     })
 
@@ -41,7 +40,7 @@ router.get('/edit/:userId',
       res.render('editProviderProfile')
     })
 
-router.post('/',
+router.post('/form',
     isLoggedIn,
     async (req,res,next) => {
         await Provider.deleteMany({userId:req.user._id})
@@ -60,5 +59,32 @@ router.post('/',
         //res.render("todoVerification")
         res.redirect('/profile/' + req.user._id)
     });
+
+  router.post('/edit',
+      isLoggedIn,
+      async (req,res,next) => {
+          const provider = Provider.findOneAndUpdate({userID:req.user._id},
+            {$set: {  providerName: req.body.providerName,
+                      providerPronouns: req.body.providerPronouns,
+                      providerEducation: req.body.providerEducation,
+                      providerOrganization: req.body.providerOrganization,
+                      providerAddress: req.body.providerAddress,
+                      providerPhone: req.body.providerPhone,
+                      providerEmail: req.body.providerEmail,
+                      providerBio: req.body.providerBio,
+                      userId: req.user._id
+            }}, {new: true},
+                function (err, docs) {
+                  if (err){
+                    console.log(err)
+                  }
+                  else {
+                    console.log("Original Doc : ",docs);
+                  }
+            });
+            console.log(provider)
+          //res.render("todoVerification")
+          res.redirect('/profile/' + req.user._id)
+      });
 
 module.exports = router;
